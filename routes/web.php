c2f2dcaed1a2e186ae8ca0323cfdd627f66b9ec1
RahmaFahrurrozi <zfahrur150105@gmail.com>
2024-07-02 13:47:37 +0700

    instalasi laravel

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/umkm', function () {
-    return view('umkm'); 
-})->name('umkm');
-
-Route::get('/produk', function () {
-    return view('produk'); 
-})->name('produk');
-
-Route::get('/berita', function () {
-    return view('berita'); 
-})->name('berita');
+    return view('welcome');
+});

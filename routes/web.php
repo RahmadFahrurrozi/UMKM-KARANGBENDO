021f034665571316ccfcf7d189182ca613a35f57
RahmaFahrurrozi <zfahrur150105@gmail.com>
2024-07-02 09:08:54 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/umkm', function () {
+    return view('umkm'); 
+})->name('umkm');
+
+Route::get('/produk', function () {
+    return view('produk'); 
+})->name('produk');
+
+Route::get('/berita', function () {
+    return view('berita'); 
+})->name('berita');
